b39dd82383d2cd5d475780de4aa6b2b56b986be9
Quinn Stephens <quinn@quinnstephens.com>
2020-06-30 11:15:52 -0500

    Throttle update and do not mutate state

--- src/App.js
+++ src/App.js
@@ -10,21 +10,23 @@ function App() {
   const [ballVelocity, setBallVelocity] = useState({ x: 0.5, y: 0.5 })
   const [ballPos, setBallPos] = useState({ left: 100, top: 100 })
   function update(delta) {
-    let newBallVelocity = ballVelocity
     let left = ballPos.left + ballVelocity.x * delta
     let top = ballPos.top + ballVelocity.y * delta
     left = Math.min(WIDTH - BALL_DIAMETER, Math.max(0, left))
     top = Math.min(HEIGHT - BALL_DIAMETER, Math.max(0, top))
+    setBallPos({ left, top })
+
+    let newBallVelocity = { ...ballVelocity }
     if (left >= WIDTH - BALL_DIAMETER || left <= 0) {
       newBallVelocity.x = -ballVelocity.x
     }
     if (top >= HEIGHT - BALL_DIAMETER || top <= 0) {
       newBallVelocity.y = -ballVelocity.y
     }
-    setBallPos({ left, top })
+    setBallVelocity(newBallVelocity)
   }
   useEffect(() => {
-    const t = d3.timer(update)
+    const t = d3.timer(update, 5)
     return () => t.stop()
   })
   return (
